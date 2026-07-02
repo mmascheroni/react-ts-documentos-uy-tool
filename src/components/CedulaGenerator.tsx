5c2fc0e68f18150c6051e3e33a920dbddc22c8c7
Mauro Mascheroni <mauromascheronipardo@gmail.com>
2026-07-02 18:29:18 -0300

    corrigiendo configuraciones

--- src/components/CedulaGenerator.tsx
+++ src/components/CedulaGenerator.tsx
@@ -1,17 +1,11 @@
 import { useState } from "react";
-import { generarCedula, generarCedulaAleatoria } from "../services/ci";
+import { generarCedulaAleatoria } from "../services/ci";
 
-import { Input } from "../components/Input";
 import { Button } from "../components/Button";
 import { Card } from "../components/Card";
 
 export function CedulaGenerator() {
-    const [numero, setNumero] = useState("");
     const [cedula, setCedula] = useState("");
-
-    function handleGenerate() {
-        setCedula(generarCedula(numero));
-    }
 
     function handleRandom() {
         setCedula(generarCedulaAleatoria());
@@ -19,12 +13,6 @@ export function CedulaGenerator() {
 
     return (
         <div className="space-y-4">
-
-            {/* <Input
-                value={numero}
-                onChange={(e) => setNumero(e.target.value)}
-                placeholder="Ej: 1234567"
-            /> */}
 
 
             <Button onClick={handleRandom} variant="primary">
--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,5 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
+  base: "react-ts-documentos-uy-tool",
 })
